272d250a1e31b25120559b751737b6f0e059b79c
Gangadhar DN <44730031+Gangadharbhuvan@users.noreply.github.com>
2021-01-17 21:42:29 +0530

    Added Day-16 Solution

---
+++ 16.Kth_LArgest_Element_in_an_Array/16.Kth_LArgest_Element_in_an_Array.py
@@ -0,0 +1,34 @@
+'''
+    Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+
+
+'''
+
+class Solution:
+    def findKthLargest(self, nums, k):
+        if not nums: return
+        pivot = random.choice(nums)
+        left =  [x for x in nums if x > pivot]
+        mid  =  [x for x in nums if x == pivot]
+        right = [x for x in nums if x < pivot]
+        
+        L, M = len(left), len(mid)
+        
+        if k <= L:
+            return self.findKthLargest(left, k)
+        elif k > L + M:
+            return self.findKthLargest(right, k - L - M)
+        else:
+            return mid[0]
